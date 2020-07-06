6512aa9574b9225c6b3307f1297ce4b4158e4c37
gaganVirk <deep.virk@outlook.co.nz>
2020-07-07 11:26:06 +1200

    Duplicated each occurrence of zero, shifting the remaining elements to the right

--- src/Squares_of_a_sorted_array.java
+++ src/Squares_of_a_sorted_array.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class Squares_of_a_sorted_array {
-    public static int[] sortedSquares(int[] A) {
+  /*  public static int[] sortedSquares(int[] A) {
         int N = A.length;
         int[] ans = new int[N];
 
@@ -10,7 +10,38 @@ public class Squares_of_a_sorted_array {
         }
         Arrays.sort(ans);
         return ans;
-    }
+    } */
+
+  public static int[] sortedSquares(int[] A) {
+      int N = A.length;
+      int j = 0;
+      while(j < N && A[j] < 0)
+          j++;
+          int i = j - 1;
+
+          int[] ans = new int[N];
+          int t = 0;
+
+          while (i >= 0 && j < N) {
+              if(A[i] * A[i] < A[j] * A[j]) {
+                  ans[t++] = A[i] * A[i];
+                  i--;
+              } else {
+                  ans[t++] = A[j] * A[j];
+                  j++;
+              }
+          }
+          while(i >= 0) {
+              ans[t++] = A[i] * A[i];
+              i--;
+          }
+          while (j < N) {
+              ans[t++] = A[j] * A[j];
+              j++;
+          }
+          return ans;
+      
+  }
 
     public static void main(String[] args) {
         int[] nums = new int[] {-4,-1,0,3,10};
